1879e08649d9ecd4e05ea2f74b79fbf67052dfea
mohanaprasath <mohanaprasath@apzzo.com>
2026-05-21 18:52:38 +0530

    Stack issue

--- apps/doctor/src/router/App/AppStack.tsx
+++ apps/doctor/src/router/App/AppStack.tsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import navigationStrings from "../../constants/navigationStrings";
-import Home from "../../screens/App/Home";
+import BottomBarNavigation from "./BottomBarNavigation";
 import MorningBrief from "../../screens/App/Home/MorningBrief";
 import PracticeIntelligence from "../../screens/App/Home/PracticeIntelligence";
 import ReportHub from "../../screens/App/Home/PractceIntelligence/ReportHub";
@@ -87,13 +87,9 @@ const AppStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
       <Stack.Screen
-        name={navigationStrings.HOME}
-        component={Home}
-        options={{
-          headerShown: false,
-          headerShadowVisible: false,
-          headerBackButtonDisplayMode: "minimal",
-        }}
+        name={navigationStrings.BOTTOM_NAVIGATION}
+        component={BottomBarNavigation}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name={navigationStrings.MORNING_BRIEF}
